f17de08431f7dc3af6879a3db1729400df6a75ef
David Piesse <piesse@gmail.com>
2016-02-22 15:02:42 +0000

    Added Views

--- config/app.php
+++ config/app.php
@@ -156,10 +156,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 //        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
-        Laracasts\Flash\FlashServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
-        Vinkla\Hashids\HashidsServiceProvider::class,
-        Sofa\Eloquence\ServiceProvider::class,
+//        Laracasts\Flash\FlashServiceProvider::class,
+//        Barryvdh\Debugbar\ServiceProvider::class,
+//        Vinkla\Hashids\HashidsServiceProvider::class,
+//        Sofa\Eloquence\ServiceProvider::class,
     ],
 
     /*
@@ -205,9 +205,9 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Flash'     => Laracasts\Flash\Flash::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
-        'Hashids' => Vinkla\Hashids\Facades\Hashids::class
+//        'Flash'     => Laracasts\Flash\Flash::class,
+//        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+//        'Hashids' => Vinkla\Hashids\Facades\Hashids::class
 
     ],
 
